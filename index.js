d17de9f139afd07f5801b0b110a91a0e0373bb7f
Juned Ahmed Choudhary <jjahmedchoudhary@gmail.com>
2024-11-21 20:34:34 +0530

    test

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express"
+import cors from "cors"
+import helmet from "helmet"
+import cookieParser from "cookie-parser"
+import dotenv from "dotenv"
+
+dotenv.config()
+const app = express()
+
+const corsOptions = {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}
+
+app.use(cors(corsOptions))
+app.use(helmet())
+app.use(cookieParser())
+
+app.use(express.json())
+
+
+app.get("/", (req, res, next) => {
+    return res.json("Hello World")
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
